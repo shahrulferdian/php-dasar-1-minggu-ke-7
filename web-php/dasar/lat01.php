66896922f09bb04a0b45d5074a8b46c3449e4cf0
shahrulferdian <87300888+shahrulferdian@users.noreply.github.com>
2021-08-26 14:01:56 +0700

    Add files via upload

---
+++ web-php/dasar/lat01.php
@@ -0,0 +1,22 @@
+<?php 
+
+echo "belajar";
+echo "<br>";
+echo 100;
+
+echo "<br>";
+
+$huruf = "saya beljr";
+
+echo $huruf;
+
+?>
+
+<?php 
+
+echo $huruf;
+echo "<br>";
+echo $huruf;
+
+echo "<br>";
+?>
---
+++ web-php/project/content.php
@@ -0,0 +1,13 @@
+<?php 
+$titel = "belajar php";
+
+$logo = 'Shahrul F.j';
+
+$nav = [
+    'home'=>'index.php',
+    'about'=>'isi-php/about.php',
+    'service'=>'isi-php/service.php',
+    'contact'=>'isi-php/contact.php',
+    'produk'=>'#'
+];
+?>
